9c3a450e5a1c758b6225a17b7210ac940b85d028
ayush <47188199+ayus786@users.noreply.github.com>
2021-10-13 23:10:46 +0530

    Update jugglinf_algo_rotation.cpp

--- jugglinf_algo_rotation.cpp
+++ jugglinf_algo_rotation.cpp
@@ -1,12 +1,19 @@
-int n, k;
+// here steps is the __gcd(n,k) this is very necessary condition many time it will take more time when its __gcd will be one
+	// we can rotate our array in O(n) with O(1) extra space
+	int n, k;
 	cin >> n >> k;
 	vector<int> ar(n);
 	for (int i = 0; i < n; i++) cin >> ar[i];
-	for (int i = 0; i < k; i++) {
-		int temp = ar[i];
-		for (int j = i; j < n - k + i; j += k) {
-			ar[j] = ar[j + k];
+	for (int i = 0; i < __gcd(n, k); i++) {
+		int j = i, temp = ar[i], d = -1;
+		while (1) {
+			d = (j + k) % n;
+			if (d == i) {
+				break;
+			}
+			ar[j] = ar[d];
+			j = d;
 		}
-		ar[n - k + i] = temp;
+		ar[j] = temp;
 	}
 	for (auto i : ar) cout << i << " ";
